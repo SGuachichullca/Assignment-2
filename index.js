1b61b5faa4d915f5ac3f6b5014da4f431f7aa8ef
Sebastian Guachichullca <sebastian.guachichullca@cix.csi.cuny.edu>
2022-01-06 00:05:32 -0500

    yeah

--- index.js
+++ index.js
@@ -66,19 +66,22 @@ function myEvery(arr, callback){
 //////////////////////////////////////////////
 
 //reduce()
-function myReduce(arr, callback){
+function thisCallBack(element){  // very basic cb to just return element
+    return element;
+ }
+ 
+ function myReduce(arr, callback){
+    //let newarr = [];      //returning a #, why do you need a new array?
     let newarr = [];
-    var reduce = callback(newarr[0], 0);    
-        for(let i = 0; i < arr.length; i++){
-            reduce = callback(arr[i],reduce);
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        newarr[i] = callback(arr[i]);
+        sum += newarr[i];        // loop over array and get the sum
+        //num == cb(arr[i+1]);    // don't need to compare here 
         }
-        return reduce;
-}
-function sum(element, previousElement) {
-    return element + previousElement;
-}
-const reduceResult = myReduce(myReduce(arr,sum));
-console.log(reduceResult);
+        return sum;  
+ }
+    console.log(myReduce(arr,thisCallBack))
 
     
 //////////////////////////////////////////////
